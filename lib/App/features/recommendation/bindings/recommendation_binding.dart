06cd61d466a5d6b92f16246e2688f6f89a0d77f1
Awftzky <Aufaazzakyarief@gmail.com>
2025-09-04 17:18:34 +0800

    NAVIGATION Half implemented design

--- lib/App/features/recommendation/bindings/recommendation_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:sayurinaja/App/features/recommendation/controllers/recommendation_controller.dart';
-import 'package:get/get.dart';
-
-class RecommendationBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<RecommendationController>(() => RecommendationController() , fenix: true);
-  }
-}
---
+++ lib/App/shared/models/store/store_model.dart
@@ -0,0 +1,13 @@
+
+class FarmerStoreModel { /// ADAPTED VARIABEL
+  final String imagePath;
+  final String storeName;
+  final String storeLocation;
+  final String starReview;
+
+  FarmerStoreModel(
+      this.imagePath,
+      this.storeName,
+      this.storeLocation,
+      this.starReview);
+}
